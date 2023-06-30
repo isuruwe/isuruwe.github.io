6077b372404493ac853500007e02305588ddd1a7
Isuru Weerasinghe <isuruwe@gmail.com>
2023-06-30 15:57:20 +0530

    v12

--- mailing/mailingvariables.php
+++ mailing/mailingvariables.php
@@ -2,6 +2,6 @@
     $mail_host = "ssl://smtp.gmail.com";
     $mail_port = "587";
     $mail_sender_email = "isuruwe@gmail.com"; //sender
-    $mail_sender_password = "zcvuvhoegcjcxutj"; //sender
+    $mail_sender_password = ""; //sender
     $mail_sender_name = "DigiTechNow";
 ?>
